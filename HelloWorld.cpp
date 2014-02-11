d509ca40f353eabd346a5c9118c7d598214bd5db
earthperson <ponomarev.dev@gmail.com>
2014-02-11 11:50:20 +0400

    Initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	// 1
+	std::cout << "Hello world at 15 November 2011" << endl;
+
+	// 2
+	cout << "Hello, World" << endl;
+	char myLine[100];
+	cin.getline(myLine, 100);
+	cout << myLine << endl;
+
+	// 3
+	string myLine2;
+	cin >> myLine2;
+	cout << myLine2 << endl;
+
+	return 0;
+}
+
